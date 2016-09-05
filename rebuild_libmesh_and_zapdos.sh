3745d80421acb4c82e06a4bbd5327631a96bbb63
Alex Lindsay <adlinds3@ncsu.edu>
2016-09-05 14:11:29 -0500

    Update rebuild_libmesh_and_zapdos.sh script.

--- rebuild_libmesh_and_zapdos.sh
+++ rebuild_libmesh_and_zapdos.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
-PROJECTDIR=/home/lindsayad/projects
-
-cd $PROJECTDIR/zapdos
-make clobber
-METHOD=dbg make clobber
-
-cd ../moose
-git pull origin master
 git clean -fxd
-./scripts/update_and_rebuild_libmesh.sh --disable-timestamps
-
-cd ../zapdos
+cd moose
+git clean -fxd
+./scripts/update_and_rebuild_libmesh.sh
+cd ..
 make -j4
