8fd1dd419204c57716d9cbb1d507c9c4b0d3b689
Alex Lindsay <alexlindsay239@gmail.com>
2017-06-01 15:52:28 -0500

    fix keywords

--- WIPfiles/DGTotFluxContPotDriven.C
+++ WIPfiles/DGTotFluxContPotDriven.C
@@ -2,12 +2,13 @@
 
 #include <cmath>
 
-template<>
-InputParameters validParams<DGTotFluxContPotDriven>()
+template <>
+InputParameters
+validParams<DGTotFluxContPotDriven>()
 {
   InputParameters params = validParams<InterfaceKernel>();
-  params.addRequiredCoupledVar("potential","The electrical potential");
-  params.addRequiredCoupledVar("potential_neighbor","The neighboring electrical potential");
+  params.addRequiredCoupledVar("potential", "The electrical potential");
+  params.addRequiredCoupledVar("potential_neighbor", "The neighboring electrical potential");
   return params;
 }
 
@@ -21,8 +22,9 @@ DGTotFluxContPotDriven::DGTotFluxContPotDriven(const InputParameters & parameter
     _potential_neighbor_id(coupled("potential_neighbor")),
     _mu(getMaterialProperty
 {
-  if (!parameters.isParamValid("boundary")) {
-    std::cerr << "In order to use this kernel, you must specify a boundary where it will live." << std::endl;
+  if (!parameters.isParamValid("boundary"))
+  {
+    mooseError("In order to use this kernel, you must specify a boundary where it will live.");
     std::exit(1);
   }
 }
@@ -38,28 +40,34 @@ DGTotFluxContPotDriven::computeQpResidual(Moose::DGResidualType type)
   else
     is_elem = false;
 
-  const unsigned int elem_b_order = static_cast<unsigned int> (is_elem ? _var.order() : _neighbor_var.order());
-  const double h_elem = _current_elem->volume()/_current_side_elem->volume() * 1./std::pow(elem_b_order, 2.);
+  const unsigned int elem_b_order =
+      static_cast<unsigned int>(is_elem ? _var.order() : _neighbor_var.order());
+  const double h_elem =
+      _current_elem->volume() / _current_side_elem->volume() * 1. / std::pow(elem_b_order, 2.);
 
   switch (type)
   {
-  case Moose::Element:
-    r += 0.5 * (-_D * _grad_u[_qp] * _normals[_qp] * _test[_i][_qp] + _epsilon * _grad_test[_i][_qp] * _normals[_qp] * _D * _u[_qp]);
-    r += _sigma / h_elem * _u[_qp] * _test[_i][_qp];
+    case Moose::Element:
+      r += 0.5 * (-_D * _grad_u[_qp] * _normals[_qp] * _test[_i][_qp] +
+                  _epsilon * _grad_test[_i][_qp] * _normals[_qp] * _D * _u[_qp]);
+      r += _sigma / h_elem * _u[_qp] * _test[_i][_qp];
 
-    r += 0.5 * (-_D_neighbor * _grad_neighbor_value[_qp] * _normals[_qp] * _test[_i][_qp] - _epsilon * _grad_test[_i][_qp] * _normals[_qp] * _D_neighbor * _neighbor_value[_qp]);
-    r += - _sigma / h_elem * _neighbor_value[_qp] * _test[_i][_qp];
-    break;
+      r += 0.5 *
+           (-_D_neighbor * _grad_neighbor_value[_qp] * _normals[_qp] * _test[_i][_qp] -
+            _epsilon * _grad_test[_i][_qp] * _normals[_qp] * _D_neighbor * _neighbor_value[_qp]);
+      r += -_sigma / h_elem * _neighbor_value[_qp] * _test[_i][_qp];
+      break;
 
-  case Moose::Neighbor:
-    r += 0.5 * (_D * _grad_u[_qp] * _normals[_qp] + _D_neighbor * _grad_neighbor_value[_qp] * _normals[_qp]) * _test_neighbor[_i][_qp];
-    r += _epsilon * 0.5 * _grad_test_neighbor[_i][_qp] * _normals[_qp] * (_D * _u[_qp] - _D_neighbor * _neighbor_value[_qp]);
-    r -= _sigma / h_elem * (_u[_qp] - _neighbor_value[_qp]) * _test_neighbor[_i][_qp];
-    break;
+    case Moose::Neighbor:
+      r += 0.5 * (_D * _grad_u[_qp] * _normals[_qp] +
+                  _D_neighbor * _grad_neighbor_value[_qp] * _normals[_qp]) *
+           _test_neighbor[_i][_qp];
+      r += _epsilon * 0.5 * _grad_test_neighbor[_i][_qp] * _normals[_qp] *
+           (_D * _u[_qp] - _D_neighbor * _neighbor_value[_qp]);
+      r -= _sigma / h_elem * (_u[_qp] - _neighbor_value[_qp]) * _test_neighbor[_i][_qp];
+      break;
   }
 
-  // std::cout << "Inside of DGTotFluxContPotDriven residual calculation." << std::endl;
-  // std::cout << "The value of the DGTotFluxContPotDriven residual is " << r << std::endl;
   return r;
 }
 
@@ -74,37 +82,40 @@ DGTotFluxContPotDriven::computeQpJacobian(Moose::DGJacobianType type)
   else
     is_elem = false;
 
-  const unsigned int elem_b_order = static_cast<unsigned int> (is_elem ? _var.order() : _neighbor_var.order());
-  const double h_elem = _current_elem->volume()/_current_side_elem->volume() * 1./std::pow(elem_b_order, 2.);
+  const unsigned int elem_b_order =
+      static_cast<unsigned int>(is_elem ? _var.order() : _neighbor_var.order());
+  const double h_elem =
+      _current_elem->volume() / _current_side_elem->volume() * 1. / std::pow(elem_b_order, 2.);
 
   switch (type)
   {
 
-  case Moose::ElementElement:
-    r -= 0.5 * _D * _grad_phi[_j][_qp] * _normals[_qp] * _test[_i][_qp];
-    r += _epsilon * 0.5 * _grad_test[_i][_qp] * _normals[_qp] * _D * _phi[_j][_qp];
-    r += _sigma / h_elem * _phi[_j][_qp] * _test[_i][_qp];
-    break;
+    case Moose::ElementElement:
+      r -= 0.5 * _D * _grad_phi[_j][_qp] * _normals[_qp] * _test[_i][_qp];
+      r += _epsilon * 0.5 * _grad_test[_i][_qp] * _normals[_qp] * _D * _phi[_j][_qp];
+      r += _sigma / h_elem * _phi[_j][_qp] * _test[_i][_qp];
+      break;
 
-  case Moose::ElementNeighbor:
-    r -= 0.5 * _D * _grad_phi_neighbor[_j][_qp] * _normals[_qp] * _test[_i][_qp];
-    r -= _epsilon * 0.5 * _grad_test[_i][_qp] * _normals[_qp] * _D * _phi_neighbor[_j][_qp];
-    r -= _sigma / h_elem * _phi_neighbor[_j][_qp] * _test[_i][_qp];
-    break;
+    case Moose::ElementNeighbor:
+      r -= 0.5 * _D * _grad_phi_neighbor[_j][_qp] * _normals[_qp] * _test[_i][_qp];
+      r -= _epsilon * 0.5 * _grad_test[_i][_qp] * _normals[_qp] * _D * _phi_neighbor[_j][_qp];
+      r -= _sigma / h_elem * _phi_neighbor[_j][_qp] * _test[_i][_qp];
+      break;
 
-  case Moose::NeighborElement:
-    r += 0.5 * _D * _grad_phi[_j][_qp] * _normals[_qp] * _test_neighbor[_i][_qp];
-    r += _epsilon * 0.5 * _grad_test_neighbor[_i][_qp] * _normals[_qp] * _D * _phi[_j][_qp];
-    r -= _sigma / h_elem * _phi[_j][_qp] * _test_neighbor[_i][_qp];
-    break;
+    case Moose::NeighborElement:
+      r += 0.5 * _D * _grad_phi[_j][_qp] * _normals[_qp] * _test_neighbor[_i][_qp];
+      r += _epsilon * 0.5 * _grad_test_neighbor[_i][_qp] * _normals[_qp] * _D * _phi[_j][_qp];
+      r -= _sigma / h_elem * _phi[_j][_qp] * _test_neighbor[_i][_qp];
+      break;
 
-  case Moose::NeighborNeighbor:
-    r += 0.5 * _D_neighbor * _grad_phi_neighbor[_j][_qp] * _normals[_qp] * _test_neighbor[_i][_qp];
-    r -= _epsilon * 0.5 * _grad_test_neighbor[_i][_qp] * _normals[_qp] * _D_neighbor * _phi_neighbor[_j][_qp];
-    r += _sigma / h_elem * _D_neighbor * _phi_neighbor[_j][_qp] * _test_neighbor[_i][_qp];
-    break;
+    case Moose::NeighborNeighbor:
+      r +=
+          0.5 * _D_neighbor * _grad_phi_neighbor[_j][_qp] * _normals[_qp] * _test_neighbor[_i][_qp];
+      r -= _epsilon * 0.5 * _grad_test_neighbor[_i][_qp] * _normals[_qp] * _D_neighbor *
+           _phi_neighbor[_j][_qp];
+      r += _sigma / h_elem * _D_neighbor * _phi_neighbor[_j][_qp] * _test_neighbor[_i][_qp];
+      break;
   }
 
   return r;
 }
-
