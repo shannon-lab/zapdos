f50defc8e0ac1485621551eece87253e39190378
Casey Icenhour <cticenho@ncsu.edu>
2020-07-23 13:24:27 -0600

    Convert DGKernels to AD

--- src/dgkernels/DGCoeffDiffusion.C
+++ src/dgkernels/DGCoeffDiffusion.C
@@ -10,18 +10,12 @@
 
 #include "DGCoeffDiffusion.h"
 
-// MOOSE includes
-#include "MooseVariable.h"
-
-#include <cmath>
-
 registerMooseObject("ZapdosApp", DGCoeffDiffusion);
 
-template <>
 InputParameters
-validParams<DGCoeffDiffusion>()
+DGCoeffDiffusion::validParams()
 {
-  InputParameters params = validParams<DGKernel>();
+  InputParameters params = ADDGKernel::validParams();
   // See header file for sigma and epsilon
   params.addRequiredParam<Real>("sigma", "sigma");
   params.addRequiredParam<Real>("epsilon", "epsilon");
@@ -31,18 +25,18 @@ validParams<DGCoeffDiffusion>()
 }
 
 DGCoeffDiffusion::DGCoeffDiffusion(const InputParameters & parameters)
-  : DGKernel(parameters),
+  : ADDGKernel(parameters),
     _epsilon(getParam<Real>("epsilon")),
     _sigma(getParam<Real>("sigma")),
-    _diff(getMaterialProperty<Real>("diff" + _var.name())),
-    _diff_neighbor(getNeighborMaterialProperty<Real>("diff" + _var.name()))
+    _diff(getADMaterialProperty<Real>("diff" + _var.name())),
+    _diff_neighbor(getNeighborADMaterialProperty<Real>("diff" + _var.name()))
 {
 }
 
-Real
+ADReal
 DGCoeffDiffusion::computeQpResidual(Moose::DGResidualType type)
 {
-  Real r = 0;
+  ADReal r = 0;
 
   const unsigned int elem_b_order = static_cast<unsigned int>(_var.order());
   const double h_elem =
@@ -70,62 +64,6 @@ DGCoeffDiffusion::computeQpResidual(Moose::DGResidualType type)
       r += _epsilon * 0.5 * _grad_test_neighbor[_i][_qp] * _normals[_qp] *
            (std::exp(_u[_qp]) - std::exp(_u_neighbor[_qp]));
       r -= _sigma / h_elem * (std::exp(_u[_qp]) - std::exp(_u_neighbor[_qp])) *
-           _test_neighbor[_i][_qp];
-      break;
-  }
-
-  return r;
-}
-
-Real
-DGCoeffDiffusion::computeQpJacobian(Moose::DGJacobianType type)
-{
-  Real r = 0;
-
-  const unsigned int elem_b_order = static_cast<unsigned int>(_var.order());
-  const double h_elem =
-      _current_elem->volume() / _current_side_elem->volume() * 1. / std::pow(elem_b_order, 2.);
-
-  switch (type)
-  {
-
-    case Moose::ElementElement:
-      r -= 0.5 * _diff[_qp] *
-           (std::exp(_u[_qp]) * _grad_phi[_j][_qp] +
-            std::exp(_u[_qp]) * _phi[_j][_qp] * _grad_u[_qp]) *
-           _normals[_qp] * _test[_i][_qp];
-      r += _epsilon * 0.5 * _grad_test[_i][_qp] * _normals[_qp] * std::exp(_u[_qp]) * _phi[_j][_qp];
-      r += _sigma / h_elem * std::exp(_u[_qp]) * _phi[_j][_qp] * _test[_i][_qp];
-      break;
-
-    case Moose::ElementNeighbor:
-      r -= 0.5 * _diff_neighbor[_qp] *
-           (std::exp(_u_neighbor[_qp]) * _grad_phi_neighbor[_j][_qp] +
-            std::exp(_u_neighbor[_qp]) * _phi_neighbor[_j][_qp] * _grad_u_neighbor[_qp]) *
-           _normals[_qp] * _test[_i][_qp];
-      r -= _epsilon * 0.5 * _grad_test[_i][_qp] * _normals[_qp] * std::exp(_u_neighbor[_qp]) *
-           _phi_neighbor[_j][_qp];
-      r -= _sigma / h_elem * std::exp(_u_neighbor[_qp]) * _phi_neighbor[_j][_qp] * _test[_i][_qp];
-      break;
-
-    case Moose::NeighborElement:
-      r += 0.5 * _diff[_qp] *
-           (std::exp(_u[_qp]) * _grad_phi[_j][_qp] +
-            std::exp(_u[_qp]) * _phi[_j][_qp] * _grad_u[_qp]) *
-           _normals[_qp] * _test_neighbor[_i][_qp];
-      r += _epsilon * 0.5 * _grad_test_neighbor[_i][_qp] * _normals[_qp] * std::exp(_u[_qp]) *
-           _phi[_j][_qp];
-      r -= _sigma / h_elem * std::exp(_u[_qp]) * _phi[_j][_qp] * _test_neighbor[_i][_qp];
-      break;
-
-    case Moose::NeighborNeighbor:
-      r += 0.5 * _diff_neighbor[_qp] *
-           (std::exp(_u_neighbor[_qp]) * _grad_phi_neighbor[_j][_qp] +
-            std::exp(_u_neighbor[_qp]) * _phi_neighbor[_j][_qp] * _grad_u_neighbor[_qp]) *
-           _normals[_qp] * _test_neighbor[_i][_qp];
-      r -= _epsilon * 0.5 * _grad_test_neighbor[_i][_qp] * _normals[_qp] *
-           std::exp(_u_neighbor[_qp]) * _phi_neighbor[_j][_qp];
-      r += _sigma / h_elem * std::exp(_u_neighbor[_qp]) * _phi_neighbor[_j][_qp] *
            _test_neighbor[_i][_qp];
       break;
   }
--- src/dgkernels/DGEFieldAdvection.C
+++ src/dgkernels/DGEFieldAdvection.C
@@ -10,16 +10,12 @@
 
 #include "DGEFieldAdvection.h"
 
-// MOOSE includes
-#include "MooseVariable.h"
-
 registerMooseObject("ZapdosApp", DGEFieldAdvection);
 
-template <>
 InputParameters
-validParams<DGEFieldAdvection>()
+DGEFieldAdvection::validParams()
 {
-  InputParameters params = validParams<DGKernel>();
+  InputParameters params = ADDGKernel::validParams();
   params.addRequiredCoupledVar("potential", "The potential that drives advection.");
   params.addClassDescription(
       "The discontinuous Galerkin form of the generic electric field driven advection term"
@@ -28,30 +24,27 @@ validParams<DGEFieldAdvection>()
 }
 
 DGEFieldAdvection::DGEFieldAdvection(const InputParameters & parameters)
-  : DGKernel(parameters),
+  : ADDGKernel(parameters),
 
-    _mu(getMaterialProperty<Real>("mu" + _var.name())),
+    _mu(getADMaterialProperty<Real>("mu" + _var.name())),
     _sgn(getMaterialProperty<Real>("sgn" + _var.name())),
-    _mu_neighbor(getNeighborMaterialProperty<Real>("mu" + _var.name())),
+    _mu_neighbor(getNeighborADMaterialProperty<Real>("mu" + _var.name())),
     _sgn_neighbor(getNeighborMaterialProperty<Real>("sgn" + _var.name())),
 
     _potential_var(*getVar("potential", 0)),
-    _potential_id(coupled("potential")),
-    _grad_potential(coupledGradient("potential")),
-    _grad_potential_neighbor(_potential_var.gradSlnNeighbor()),
-    _grad_phi_pot(_potential_var.gradPhiFace()),
-    _grad_phi_neighbor_pot(_potential_var.gradPhiFaceNeighbor())
+    _grad_potential(adCoupledGradient("potential")),
+    _grad_potential_neighbor(_potential_var.adGradSlnNeighbor())
 {
 }
 
-Real
+ADReal
 DGEFieldAdvection::computeQpResidual(Moose::DGResidualType type)
 {
-  Real r = 0;
-  RealVectorValue _velocity = _mu[_qp] * _sgn[_qp] * -_grad_potential[_qp];
-  RealVectorValue _velocity_neighbor =
+  ADReal r = 0;
+  ADRealVectorValue _velocity = _mu[_qp] * _sgn[_qp] * -_grad_potential[_qp];
+  ADRealVectorValue _velocity_neighbor =
       _mu_neighbor[_qp] * _sgn_neighbor[_qp] * -_grad_potential_neighbor[_qp];
-  RealVectorValue _velocity_average = 0.5 * (_velocity + _velocity_neighbor);
+  ADRealVectorValue _velocity_average = 0.5 * (_velocity + _velocity_neighbor);
 
   switch (type)
   {
@@ -71,107 +64,4 @@ DGEFieldAdvection::computeQpResidual(Moose::DGResidualType type)
       break;
   }
   return r;
-}
-
-Real
-DGEFieldAdvection::computeQpJacobian(Moose::DGJacobianType type)
-{
-  Real jac = 0;
-  RealVectorValue _velocity = _mu[_qp] * _sgn[_qp] * -_grad_potential[_qp];
-  RealVectorValue _velocity_neighbor =
-      _mu_neighbor[_qp] * _sgn_neighbor[_qp] * -_grad_potential_neighbor[_qp];
-  RealVectorValue _velocity_average = 0.5 * (_velocity + _velocity_neighbor);
-
-  switch (type)
-  {
-
-    case Moose::ElementElement:
-      if ((_velocity_average * _normals[_qp]) >= 0.0)
-        jac += (_velocity_average * _normals[_qp]) * std::exp(_u[_qp]) * _phi[_j][_qp] *
-               _test[_i][_qp];
-      else
-        jac += 0.0;
-      break;
-
-    case Moose::ElementNeighbor:
-      if ((_velocity_average * _normals[_qp]) >= 0.0)
-        jac += 0.0;
-      else
-        jac += (_velocity_average * _normals[_qp]) * std::exp(_u_neighbor[_qp]) *
-               _phi_neighbor[_j][_qp] * _test[_i][_qp];
-      break;
-
-    case Moose::NeighborElement:
-      if ((_velocity_average * _normals[_qp]) >= 0.0)
-        jac += -(_velocity_average * _normals[_qp]) * std::exp(_u[_qp]) * _phi[_j][_qp] *
-               _test_neighbor[_i][_qp];
-      else
-        jac += 0.0;
-      break;
-
-    case Moose::NeighborNeighbor:
-      if ((_velocity_average * _normals[_qp]) >= 0.0)
-        jac += 0.0;
-      else
-        jac += -(_velocity_average * _normals[_qp]) * std::exp(_u_neighbor[_qp]) *
-               _phi_neighbor[_j][_qp] * _test_neighbor[_i][_qp];
-      break;
-  }
-  return jac;
-}
-
-Real
-DGEFieldAdvection::computeQpOffDiagJacobian(Moose::DGJacobianType type, unsigned int jvar)
-{
-  Real jac = 0;
-
-  if (jvar == _potential_id)
-  {
-    RealVectorValue _velocity = _mu[_qp] * _sgn[_qp] * -_grad_potential[_qp];
-    RealVectorValue _velocity_neighbor =
-        _mu_neighbor[_qp] * _sgn_neighbor[_qp] * -_grad_potential_neighbor[_qp];
-    RealVectorValue _velocity_average = 0.5 * (_velocity + _velocity_neighbor);
-    RealVectorValue _d_velocity_avg_elem = 0.5 * _mu[_qp] * _sgn[_qp] * -_grad_phi[_j][_qp];
-    RealVectorValue _d_velocity_avg_neigh =
-        0.5 * _mu_neighbor[_qp] * _sgn_neighbor[_qp] * -_grad_phi_neighbor[_j][_qp];
-
-    switch (type)
-    {
-
-      case Moose::ElementElement:
-        if ((_velocity_average * _normals[_qp]) >= 0.0)
-          jac += (_d_velocity_avg_elem * _normals[_qp]) * std::exp(_u[_qp]) * _test[_i][_qp];
-        else
-          jac +=
-              (_d_velocity_avg_elem * _normals[_qp]) * std::exp(_u_neighbor[_qp]) * _test[_i][_qp];
-        break;
-
-      case Moose::ElementNeighbor:
-        if ((_velocity_average * _normals[_qp]) >= 0.0)
-          jac += (_d_velocity_avg_neigh * _normals[_qp]) * std::exp(_u[_qp]) * _test[_i][_qp];
-        else
-          jac +=
-              (_d_velocity_avg_neigh * _normals[_qp]) * std::exp(_u_neighbor[_qp]) * _test[_i][_qp];
-        break;
-
-      case Moose::NeighborElement:
-        if ((_velocity_average * _normals[_qp]) >= 0.0)
-          jac +=
-              -(_d_velocity_avg_elem * _normals[_qp]) * std::exp(_u[_qp]) * _test_neighbor[_i][_qp];
-        else
-          jac += -(_d_velocity_avg_elem * _normals[_qp]) * std::exp(_u_neighbor[_qp]) *
-                 _test_neighbor[_i][_qp];
-        break;
-
-      case Moose::NeighborNeighbor:
-        if ((_velocity_average * _normals[_qp]) >= 0.0)
-          jac += -(_d_velocity_avg_neigh * _normals[_qp]) * std::exp(_u[_qp]) *
-                 _test_neighbor[_i][_qp];
-        else
-          jac += -(_d_velocity_avg_neigh * _normals[_qp]) * std::exp(_u_neighbor[_qp]) *
-                 _test_neighbor[_i][_qp];
-        break;
-    }
-  }
-  return jac;
 }
