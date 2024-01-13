6fe5a1f4893c610c2777067e5558ab9ae3f5d7ac
MengnanLi91 <118846840+MengnanLi91@users.noreply.github.com>
2024-01-12 17:34:44 -0700

    Clean up main.C and explicitly register dependent app #228

--- src/main.C
+++ src/main.C
@@ -9,28 +9,12 @@
 //* https://www.gnu.org/licenses/lgpl-2.1.html
 
 #include "ZapdosTestApp.h"
-#include "MooseInit.h"
-#include "Moose.h"
-#include "MooseApp.h"
-#include "AppFactory.h"
-
-// Create a performance log
-PerfLog Moose::perf_log("Zapdos");
+#include "MooseMain.h"
 
 // Begin the main program.
 int main(int argc, char *argv[])
 {
-  // Initialize MPI, solvers and MOOSE
-  MooseInit init(argc, argv);
-
-  // Register this application's MooseApp and any it depends on
-  ZapdosTestApp::registerApps();
-
-  // This creates dynamic memory that we're responsible for deleting
-  std::shared_ptr<MooseApp> app = AppFactory::createAppShared("ZapdosTestApp", argc, argv);
-
-  // Execute the application
-  app->run();
+  Moose::main<ZapdosTestApp>(argc, argv);
 
   return 0;
 }
